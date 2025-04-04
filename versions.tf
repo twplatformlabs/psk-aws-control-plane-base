2efb116ba79eaa07f997fc969ab2654a9f587514
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-04-03 22:12:27 -0500

    [nc] update hashi provider aws ~> 5.94
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.83"
+      version = "~> 5.94"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.35"
+      version = "~> 2.36"
     }
     helm = {
       source  = "hashicorp/helm"
