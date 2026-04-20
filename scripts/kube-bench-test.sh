2cdb69000961f2cc17ae70917d25021f75bce3b0
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-04-19 21:50:24 -0500

    [nc] tests will cleanup a prior failure
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/kube-bench-test.sh
+++ scripts/kube-bench-test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+if kubectl get job kube-bench -n psk-system &>/dev/null; then
+  echo "Found leftover kube-bench job, deleting..."
+  kubectl delete job kube-bench -n psk-system --wait=true
+fi
+
 kubectl apply -f test/kube-bench/job-eks.yaml
 sleep 30
 
